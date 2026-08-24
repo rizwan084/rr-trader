a7d8e557988d7cd52e39f2f32e225cc8a307f1ca
rizwan084 <rairizwan084@gmail.com>
2026-08-25 02:44:31 +0500

    Add BitGuru dashboard service worker

---
+++ dashboard/sw.js
@@ -0,0 +1,5 @@
+const CACHE='bitguru-dashboard-v1';
+const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;if(e.request.url.includes('/rest/v1/'))return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy));return r}).catch(()=>caches.match('./index.html'))))});
