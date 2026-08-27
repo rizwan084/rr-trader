607fb57c9f9a5e93b41d9735033e3fec5225fc84
rizwan084 <rairizwan084@gmail.com>
2026-08-28 03:19:33 +0500

    Add RR Trader service worker

---
+++ frontend/rr-trader-sw.js
@@ -0,0 +1,1 @@
+const CACHE='rr-trader-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/rr-trader/manifest.json','/rr-trader/icon.svg']))));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))})
